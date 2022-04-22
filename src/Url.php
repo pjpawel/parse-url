b9bb18b8d825c0838b4b18d09eb56da9fc942d3e
pjpawel <pawel.jan.podgorski@gmail.com>
2022-04-22 20:04:46 +0200

    correct url file

--- src/Url.php
+++ src/Url.php
@@ -2,13 +2,64 @@
 
 namespace pjpawel;
 
+use pjpawel\Exceptions\ParseException;
+
 class Url
 {
 
-
-    public static function parse(string $url)
+    /**
+     * @param string $url
+     * @param int $component default = -1
+     * @return array
+     * @throws ParseException
+     */
+    public static function parse(string $url, int $component = -1): array
     {
-        
+        if (preg_match('/^http(s)?:\/\/|^\/\//', $url, )) {
+            return parse_url($url, $component);
+        } elseif (preg_match('/^file/', $url)) {
+            return self::parseFile($url, $component);
+        } else {
+            /*
+             * mailto:
+             * tel:
+             * /cos/tam
+             *
+             */
+            throw new ParseException('Incorrect url');
+        }
     }
 
+    /**
+     * @param string $url
+     * @param int $component
+     * @return array
+     * @throws ParseException
+     */
+    private static function parseFile(string $url, int $component): array
+    {
+        if (preg_match('/^file:\/\/\//', $url)) {
+            return parse_url($url, $component);
+        } elseif (preg_match('/^file:\/\//', $url)) {
+            return parse_url(self::correctFile($url, 1), $component);
+        } elseif (preg_match('/^file:\//', $url)) {
+            return parse_url(self::correctFile($url, 2), $component);
+        } elseif (preg_match('/^file:/', $url)) {
+            return parse_url(self::correctFile($url, 3), $component);
+        } else {
+            throw new ParseException('Incorrect file uri');
+        }
+    }
+
+    /**
+     * @param string $url
+     * @param int $slashNumber
+     * @return string
+     */
+    private static function correctFile(string $url, int $slashNumber): string
+    {
+        $scheme = "file:";
+        $scheme .= str_repeat("/", $slashNumber);
+        return preg_replace('/^file:\/\/|^file:\/|^file:/', $scheme, $url, 1);
+    }
 }
